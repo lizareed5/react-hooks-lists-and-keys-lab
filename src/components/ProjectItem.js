7ef5e9eab406ff88c0affe26feaea8cfe0bbed15
Liza McLain <lizamclain@gmail.com>
2023-01-09 11:54:59 -0600

    commit tree cc8bad2ad8144272cf13ccbc28ca1b8a87d74bb5

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,17 @@
 import React from "react";
+// import user from "../data/user";
 
 function ProjectItem({ name, about, technologies }) {
+  
+  const technologyLi = technologies.map((technology) => (
+    <span key={technology}>{technology}</span>
+  ));
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologyLi}</div>
     </div>
   );
 }
